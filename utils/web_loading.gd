b45ef3f760bb7ff81aef8b9acf2caaa72a974c8c
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-02-02 23:27:16 +0100

    fix web loading

--- utils/web_loading.gd
+++ utils/web_loading.gd
@@ -1,7 +1,6 @@
 extends Control
 
-@export var objects_paths_to_load : Array[String] = []
-static var preloaded_objects : Array[Resource] = []
+@export var scenes_to_preload : Array[PackedScene]
 @export_file("*.tscn") var main_scene = ""
 
 ## The whole purpose of this scene is to preload assets that are going to be used later on in the game.
@@ -10,16 +9,19 @@ static var preloaded_objects : Array[Resource] = []
 signal complete
 
 func _ready():
-	$VBoxContainer/ProgressBar.max_value = objects_paths_to_load.size()
+	$VBoxContainer/ProgressBar.max_value = scenes_to_preload.size()
 	$VBoxContainer/ProgressBar.value = 0
-	await get_tree().process_frame
+	await get_tree().create_timer(0.5).timeout
 	run()
 
 func run():
-	for i in range(objects_paths_to_load.size()):
-		$VBoxContainer/ProgressBar.value = i
-		preloaded_objects.push_back(load(objects_paths_to_load[i]))
-	$KarmaParticle.visible = true
+	$VBoxContainer/LoadInfo.show()
+	for i in range(scenes_to_preload.size()):
+		$VBoxContainer/LoadInfo.text = "Loading " + \
+				scenes_to_preload[i].resource_path + "..."
+		await get_tree().create_timer(1).timeout
+		$LoadedStorage.add_child(scenes_to_preload[i].instantiate())
+		$VBoxContainer/ProgressBar.value = i + 1
 	await get_tree().create_timer(0.5).timeout
 	complete.emit()
 	await get_tree().process_frame
