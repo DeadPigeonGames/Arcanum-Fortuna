6883fdc563c359066409f3f2295139f06545e7f0
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-02-03 02:01:24 +0100

    fix enemy player

--- systems/card_combat/enemy_player.gd
+++ systems/card_combat/enemy_player.gd
@@ -77,7 +77,7 @@ func take_damage(amount, _source = null):
 func restore_default_color():
 	%HealthLabel.text = str(health)
 	%Health.modulate = Color.WHITE
-	%KarmaLabel.modulate = Color.WHITE
+	%Karma.modulate = Color.WHITE
 
 func process_death() -> bool:
 	if health < 0:
