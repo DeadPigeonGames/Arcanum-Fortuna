44f6cdc0215b8c1dc7f39ad04ce595db22031e93
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-02-12 20:26:09 +0100

    added new shop / added victorious label

--- systems/dungeon_exploration/nodes/shop_node.gd
+++ systems/dungeon_exploration/nodes/shop_node.gd
@@ -2,8 +2,7 @@ class_name ShopNode
 extends EventNode
 
 func _trigger_event():
-	var shop = event.instantiate() as ShopKeeper
-	shop.seed = seed
-	SceneHandler.add_shelf_element(shop)
-	shop.setup(player.data)
-	await shop.close_shop
+	var shop = SceneHandler.add_ui_element("res://systems/ui/menus/shop/shop.tscn") as Shop
+	shop.init(0, self)
+	shop.setup()
+	await shop.shop_closed
--- systems/ui/menus/shop/shop.gd
+++ systems/ui/menus/shop/shop.gd
@@ -1,6 +1,8 @@
 class_name Shop
 extends UIBase
 
+signal shop_closed
+
 @export var shop_buy_tab : PackedScene
 @export var shop_trade_tab : PackedScene
 @export var shop_burn_tab : PackedScene
@@ -32,4 +34,5 @@ func _on_burn_section_button_button_up():
 
 
 func _on_leave_shop_button_button_up():
+	shop_closed.emit()
 	close()
