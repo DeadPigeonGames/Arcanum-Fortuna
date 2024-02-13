0375ff9678fc35b790a2aa92b73c05aa73fc617a
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-02-13 23:41:55 +0100

    fixed poppin popup
    i wanna pop (my pop-schutz)

--- systems/ui/menus/shop/shop_burn_section.gd
+++ systems/ui/menus/shop/shop_burn_section.gd
@@ -1,3 +1,20 @@
 class_name ShopBurnSection
 extends UITabBase
 
+@export var card_prize := 4
+@export var all_cards_resource : AllCards
+@export var burn_card_1 : ShopPreviewCard
+
+var player_data : PlayerData
+var burn_button
+ 
+
+func setup():
+	player_data = Player.instance.data
+	burn_button = $BurnButton
+	burn_card_1.card_data = all_cards_resource.all_cards.pick_random()
+	burn_button.disabled = player_data.currency < card_prize
+
+
+func bla():
+	pass
