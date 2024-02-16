ceaefce71ab101767dcf132c0d13b6599e93636f
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-02-16 16:16:29 +0100

    fix web loading, improve credits

--- utils/web_loading.gd
+++ utils/web_loading.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var scenes_to_preload : Array[PackedScene]
+@export var karma_particle : PackedScene
 @export_file("*.tscn") var main_scene = ""
 
 ## The whole purpose of this scene is to preload assets that are going to be used later on in the game.
@@ -9,17 +10,18 @@ extends Control
 signal complete
 
 func _ready():
-	$VBoxContainer/ProgressBar.max_value = scenes_to_preload.size()
-	$VBoxContainer/ProgressBar.value = 0
+	#%ProgressBar.max_value = scenes_to_preload.size()
+	#%ProgressBar.value = 0
 	await get_tree().create_timer(0.5).timeout
 	run()
 
 func run():
-	$VBoxContainer/LoadInfo.show()
+	%LoadInfo.show()
 	for i in range(scenes_to_preload.size()):
-		$VBoxContainer/LoadInfo.text = "Loading " + \
+		%LoadInfo.text = "Loading " + \
 				scenes_to_preload[i].resource_path + "..."
-		$LoadedStorage.add_child(scenes_to_preload[i].instantiate())
-		$VBoxContainer/ProgressBar.value = i + 1
+		%LoadedStorage.add_child(scenes_to_preload[i].instantiate())
+		await get_tree().process_frame
+	await $ColorRect/ProgressBar/AnimationPlayer.animation_finished
 	complete.emit()
 	queue_free()
