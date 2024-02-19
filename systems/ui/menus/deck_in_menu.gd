d42680d9b089da8833e90c6502e397b2af7d4832
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-02-19 08:43:59 +0100

    simplyfied code - this one was haunting me in my dreams

--- systems/ui/menus/deck_in_menu.gd
+++ systems/ui/menus/deck_in_menu.gd
@@ -27,9 +27,7 @@ func setup():
 
 func _on_button_button_up():
 	if not deck_preview:
-		deck_preview = SceneHandler.add_ui_element(deck_preview_overlay) as DeckPreviewOverlay
-		deck_preview.init(75, self)
-		deck_preview.setup()
+		call_ui_element(deck_preview_overlay)
 	else:
 		await deck_preview.close()
 		deck_preview = null
