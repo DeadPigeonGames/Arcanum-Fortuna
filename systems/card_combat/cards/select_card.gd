2bb53913f5abe8834f7e5802c36cdfb53e096275
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2024-02-28 23:40:56 +0100

    Canvas input layer 75 changed to 80 - card inspection bug fix

--- systems/card_combat/cards/select_card.gd
+++ systems/card_combat/cards/select_card.gd
@@ -14,7 +14,7 @@ var is_selectable = true
 func _input(event):
 	if is_hovered and event.is_action_pressed("open_inspection"):
 		var new_inspection = inspection.instantiate() as CardInspection
-		new_inspection.init(75, self)
+		new_inspection.init(80, self)
 		new_inspection.inspection_init(self)
 		SceneHandler.add_ui_element(new_inspection)
 	
