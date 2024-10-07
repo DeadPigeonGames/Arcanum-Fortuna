371065624be4cbbf3c7259977fd6db47c5f70412
laskophria <valhalladancehalla@gmail.com>
2024-10-07 21:27:40 +0200

    added animation to loot event

---
+++ systems/dungeon_events/coin_anim.gd
@@ -0,0 +1,12 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play("money_bag")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
