d06959966c1f3bda3f897af10ca65bee411541e6
LumiToad <129980440+LumiToad@users.noreply.github.com>
2024-10-08 22:26:19 +0200

    Fixed Snow Particles
    - will nix hören

--- systems/dungeon_exploration/maps/level.gd
+++ systems/dungeon_exploration/maps/level.gd
@@ -5,3 +5,8 @@ extends Node
 
 func init(rng_seed, rng_text):
 	get_node(node_map_path).init(rng_seed, rng_text)
+
+
+func _process(delta: float) -> void:
+	%SnowParticles.global_position.y = 0
+	%SnowParticles.global_position.x = %LimitedCamera.global_position.x + %LimitedCamera.offset.x
